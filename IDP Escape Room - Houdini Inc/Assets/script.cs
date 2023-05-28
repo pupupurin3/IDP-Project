3ab4cce48a035e7b76591e23335966b01048993d
bonkacardonut <biancacardona.olg@gmail.com>
2023-05-27 22:17:38 -0700

    cutscene 1 & start of paintbrush puzzle

--- IDP Escape Room - Houdini Inc/Assets/script.cs
+++ IDP Escape Room - Houdini Inc/Assets/script.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour
+public class DragToFade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject targetObject;
+    public float fadeSpeed = 0.5f;
+    public float minOpacity = 0.2f;
+    public float maxOpacity = 1f;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isDragging = false;
+
+    private void Start()
     {
-        
+        // Get the SpriteRenderer component of the target object
+        spriteRenderer = targetObject.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        isDragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            // Gradually increase the transparency of the target object
+            Color currentColor = spriteRenderer.color;
+            float newOpacity = Mathf.Clamp(currentColor.a + fadeSpeed * Time.deltaTime, minOpacity, maxOpacity);
+            spriteRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, newOpacity);
+        }
     }
 }
